fa1be94b5cbf6804d84a5c81473cca58e3f86dc9
ParnianFattahi <148445422+ParnianFattahi@users.noreply.github.com>
2023-10-19 03:15:25 -0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,16 @@
+Name=input('enter your name:  ')
+lastname=input('enter your last name:  ')
+
+a=int(input('enter grade 1:  '))
+b=int(input('enter grade 2:  '))
+c=int(input('enter grade 3:  '))
+
+ave=(a+b+c)/3
+
+if ave>=17:
+    print('greate')
+if 17>ave>=12:
+    print('normal')
+else:
+    12>ave 
+    print('fail')
